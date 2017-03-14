fb87cd07bcf1a40d8e27690f45e0ea6e6a01dd6a
Hugo Duran <hu.durannn@gmail.com>
2017-03-14 13:26:43 -0300

    :O kill me please

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 20.times do |t|
-	Company.create(name:"cocacola")
+	Company.create(name:"cocacola #{t}")
 end
 
 30.times do |t|
-	User.create(name:"hbastian", email:"hbastian@gamail.com", password:"123456")
+	User.create(name:"hbastian #{t}", email:"hbastian@gamail.com", password:"123456")
 end
 
 250.times do |t|
-	Claim.create(claim:"")		
+	Claim.create(claim:"raclamo #{t}")
+end			
